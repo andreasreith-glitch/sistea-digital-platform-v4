759856f35e12a5506a8954f08a4c7d11cceb0a88
andreasreith-glitch <andreas.reith@sisteasv.com>
2026-08-09 22:22:01 -0600

    fix: remove obsolete home content loader

--- sistea-digital-platform/src/lib/content/getHomeContent.ts
+++ sistea-digital-platform/src/lib/content/getHomeContent.ts
@@ -1,13 +1,1 @@
-import { notFound } from "next/navigation";
 
-import { homeEs } from "@/content/es/pages/home";
-
-type Locale = "es" | "en" | "de";
-
-export function getHomeContent(locale: Locale) {
-  if (locale === "es") {
-    return homeEs;
-  }
-
-  notFound();
-}
