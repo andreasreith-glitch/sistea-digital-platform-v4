231d50ecb71d75f42d57b06ffaf659c6845f5ace
andreasreith-glitch <andreas.reith@sisteasv.com>
2026-08-09 22:15:40 -0600

    fix: make home locale type self contained
    Added Locale type definition for better type safety.

--- sistea-digital-platform/src/lib/content/getHomeContent.ts
+++ sistea-digital-platform/src/lib/content/getHomeContent.ts
@@ -1,7 +1,8 @@
 import { notFound } from "next/navigation";
 
-import type { Locale } from "@/config/locales";
 import { homeEs } from "@/content/es/pages/home";
+
+type Locale = "es" | "en" | "de";
 
 export function getHomeContent(locale: Locale) {
   if (locale === "es") {
